58f0bf89c964018afc6a5e8f6c6f84c3585d972b
jbrit <pro.ajibolaojo@gmail.com>
2021-03-29 00:28:42 +0100

    Mode toggler implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch } from "antd";
+
+enum Mode {
+  App,
+  Admin,
+}
 
 const App = () => {
-  return <div className="App"></div>;
+  const [mode, setMode] = useState(Mode.App);
+
+  const onModeChange = (checked: boolean) => {
+    setMode(checked ? Mode.Admin : Mode.App);
+  };
+
+  return (
+    <div className="App">
+      <Switch
+        checkedChildren="Admin"
+        unCheckedChildren="App"
+        onChange={onModeChange}
+      />
+    </div>
+  );
 };
 
 export default App;
